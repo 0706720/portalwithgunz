9c325e0dbac4710439f7a85b3c279b09962e333f
SchumacherZak <0592695@schoolsnet.act.edu.au>
2026-02-27 15:22:37 +1100

    Moving Platform moved

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -148,7 +148,7 @@ func _physics_process(delta):
 	else:
 		anim_player.play("idle")
 	var camera = $Camera3D
-s
+
 	#move_and_slide()
 
 @rpc("call_local")
@@ -179,7 +179,7 @@ var player_health = 100
 var canThrow = true
 @onready var my_label = $Label
 
-@export var JUMP_VELOCITY := 6.5
+@export var JUMP_VELOCITY := 8.0
 @export var look_sensitivity : float = 0.006
 @export var auto_bhop := true
 
@@ -208,7 +208,7 @@ var is_charging_spin := false
 var is_spin_rolling := false
 var spin_direction := Vector3.ZERO
 
-@export var spin_camera_tilt_amount := 180.0   
+@export var spin_camera_tilt_amount := 360.0   
 @export var spin_camera_tilt_speed := 200.0
 var current_camera_tilt := 0.0
 
