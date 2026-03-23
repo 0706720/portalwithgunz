6a2eb612be791b5f08f9232979c7cd4ff5b155e6
SchumacherZak <0592695@schoolsnet.act.edu.au>
2026-03-23 11:01:02 +1100

    Hash out shotgun

--- propulsion_shotgun.gd
+++ propulsion_shotgun.gd
@@ -7,16 +7,16 @@ var spread = 15
 
 func _ready() -> void:
 	randomize()
-	for r in raycasts.get_children():
-		r.cast_to.x = randf_range(spread, -spread)
-		r.cast_to.y = randf_range(spread, -spread)
+	#for r in raycasts.get_children():
+		#r.cast_to.x = randf_range(spread, -spread)
+		#r.cast_to.y = randf_range(spread, -spread)
 
 func fire_shotgun():
 	if Input.is_action_just_pressed("Fire_shotgun"):
 		print_rich("[color=green]Success![/color]")
-	for r in raycasts.get_children():
-		r.cast_to.x = randf_range(spread, -spread)
-		r.cast_to.y = randf_range(spread, -spread)
-		if r.is_colliding():
-			if r.get_collider().is_in_group("player"):
-				r.get_collider().health -= damage
+	#for r in raycasts.get_children():
+		#r.cast_to.x = randf_range(spread, -spread)
+		#r.cast_to.y = randf_range(spread, -spread)
+		#if r.is_colliding():
+			#if r.get_collider().is_in_group("player"):
+				#r.get_collider().health -= damage
