d35e81d8ef11b9317d2cd619aa2e9e9e5b24e280
Zac-Nordsvan <0257743@schoolsnet.act.edu.au>
2026-05-21 15:53:37 +1000

    1.1.1 State Machine:
    Fixed bug where error Invalid access to property or key 'state_name' on a base object of type 'Node (run_state.gd)'.

--- Scripts/StateMachine/run_state.gd
+++ Scripts/StateMachine/run_state.gd
@@ -1,1 +1,36 @@
 extends State
+
+class_name RunState
+
+var state_name : String = "Run"
+
+var play_char : CharacterBody3D
+
+func enter(play_char_ref : CharacterBody3D):
+	#pass the play char refrence 
+	play_char = play_char_ref
+
+	verifications()
+
+	print("Entered Run")
+func verifications():
+	pass
+
+func physics_update(delta : float):
+	applies(delta)
+
+	#play_char.gravity_apply(delta)
+
+	input_management()
+
+	move(delta)
+
+func applies(delta : float):
+	pass
+
+
+func input_management():
+	pass
+
+func move(delta : float):
+	pass
